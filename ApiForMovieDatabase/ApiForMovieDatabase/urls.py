09823af69f693839727573bf6dc54e8c744f6e86
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2021-08-28 22:46:25 +0600

    model_attr_reduce

--- ApiForMovieDatabase/ApiForMovieDatabase/urls.py
+++ ApiForMovieDatabase/ApiForMovieDatabase/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('student',include('student.urls'))
+    # path('student',include('student.urls'))
 
 ]
 if settings.DEBUG:

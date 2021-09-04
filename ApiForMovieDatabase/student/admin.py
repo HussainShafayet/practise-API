25bd6c0d79ae10a7282ff228071a4e3f91793308
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2021-09-04 22:18:50 +0600

    Student_Admin value Display

--- ApiForMovieDatabase/student/admin.py
+++ ApiForMovieDatabase/student/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . models import Student
 # Register your models here.
-
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','name','reg_no','dept','session','institute']
